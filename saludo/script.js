0bd7505e0600336eb3b7f09c77bca257c9321df7
Luis Medina <luisdavid220800@gmail.com>
2022-06-17 15:25:14 -0500

    Fun: Aqui ya funcionan los archivos HTML

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,18 @@
 console.log("Se nota que todavia me extrañas");
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+  //console.log("Me parece una falta de respecto");
+//   Esto nos dice como es un comentario
+  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+console.log(frase);
+  if (document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+    //frase = "Impongo moda para las niñas!";
+    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
+  } else {
+    //frase = "Ganando como siempre";
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+  }
+  console.log("frase :" + frase);
+}
